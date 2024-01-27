8f679cd39615a915803349cfb78f3b7dca05aa8d
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-01-27 23:00:29 +0500

    1st tutorial done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+
+const port=5000;  
+app.get("/",(req,res)=>{
+    res.send("This is Learning of API")
+
+});
+
+const start=async()=>{
+
+    try{
+        
+        app.listen(port,()=>{
+           console.log( `${port} Yes I am connected`);
+        })
+    }
+    catch(err){
+        console.log("Error",err)
+    }
+}
+start();
