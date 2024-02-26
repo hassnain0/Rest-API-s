5784019b16249bbe4857a3b2d75d51d0653edf6f
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-02-26 22:33:48 +0500

    Mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const bodyparser=require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app=express();
 
 app.use(bodyparser.urlencoded({extended:true}))
@@ -8,4 +9,7 @@ app.post('/quotes',(req,res)=>{
 console.log(req.body)
 })
 
-const mongo_Client=require('mongodb').MongoClient
+  MongoClient.connect('mongodb+srv://Hassnain_Ali:hassnain@cluster0.o3gnmyd.mongodb.net/', (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+  })
