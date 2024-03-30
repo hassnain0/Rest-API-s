e878cfd0d33ed8cc32ac7ff4f5a2fa9e3317bfea
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-03-31 04:51:29 +0500

    Update Data

--- server.js
+++ server.js
@@ -5,20 +5,6 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-const update=document.querySelector('#update-button')
-
-update.addEventListener('click',_=>{
-  fetch('/quotes',{
-    method:'put',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({
-      name:'Hassnain Ali',
-      quotes:'I find your lack of faith disturbing.',
-    })
-  })
-})
 app.use(bodyParser.json());
 
 app.put('/quotes',(req,res)=>{
